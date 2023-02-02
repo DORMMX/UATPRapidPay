24fb0f07b0f2b303d50443fcef25071ae9b2afeb
DAVID OMAR ROMO MARTINEZ <omar.romo@distillery.com>
2023-02-02 09:08:20 -0600

    Create Project and architecture

---
+++ Controllers/CardController.cs
@@ -0,0 +1,32 @@
+using DBRepository;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace UATPRapidPay.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CardController : ControllerBase
+    {
+        readonly ICardRepository _cardRepository;
+
+        public CardController(ICardRepository cardRepository)
+        {
+            _cardRepository = cardRepository;
+        }
+
+        [HttpGet]        
+        public ActionResult<List<Card>> Get()
+        {
+            return Ok(_cardRepository.GetCards());
+        }
+
+        [HttpPost]        
+        public ActionResult<Card> Post(Card card)
+        {
+            if (_cardRepository.CreateCards(card))
+                return Ok(card);
+            return BadRequest();
+        }
+    }
+}
