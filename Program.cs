6257677fd21a9c94af58d43a5ad487b898f59a95
DAVID OMAR ROMO MARTINEZ <omar.romo@distillery.com>
2023-02-02 11:27:53 -0600

    Manage and Fee Card

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ICardRepository, CardRepository>();
-
+builder.Services.AddSingleton<ITransactionRepositoy, TransactionRepositoy>();
+builder.Services.AddSingleton<IBalanceRepository, BalanceRepository>();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
