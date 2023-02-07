b5dc0ba1d20ca5b720a6ad074a3df2b94dca2596
DAVID OMAR ROMO MARTINEZ <omar.romo@distillery.com>
2023-02-07 15:39:03 -0600

    Add Bonus

---
+++ Authentication/IJwtFactory.cs
@@ -0,0 +1,11 @@
+﻿using DBRepository;
+using System.Security.Claims;
+
+namespace UATPRapidPay.Authentication
+{
+    public interface IJwtFactory
+    {
+        Task<string> GenerateEncodedToken(string userName, ClaimsIdentity identity);
+        ClaimsIdentity GenerateClaimsIdentity(User user, string id);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
-using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Services;
+using System.Text;
 using UATPRapidPay.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = new ConfigurationBuilder().
+    AddJsonFile("appsettings.json")    
+    .Build();
+
+var secretKey = Encoding.ASCII.GetBytes(
+                configuration.GetValue<string>("SecretKey")
+            );
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -10,9 +19,21 @@ builder.Services.AddSingleton<ICardRepository, CardRepository>();
 builder.Services.AddSingleton<ITransactionRepositoy, TransactionRepositoy>();
 builder.Services.AddSingleton<IBalanceRepository, BalanceRepository>();
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
-builder.Services.AddAuthentication("BasicAuthentication")
-                .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>
-                ("BasicAuthentication", null);
+builder.Services.AddSingleton<IJwtFactory, JwtFactory>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAuthorization();
 var app = builder.Build();
 
@@ -20,4 +41,5 @@ var app = builder.Build();
 app.UseAuthorization();
 app.UseAuthentication();
 app.MapControllers();
+
 app.Run();
